587aa8194414970d1c432b38fe3530ec9627c007
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-13 18:40:59 +0300

    create: create a code file print string in chosine format

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
+language that combines remarkable power with very clear syntax"
 print(str[38:66] + " " + str[-22:-17] + str[:6])
