7275879e05e8c17a257964f9a15101b8cb5c5be5
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-14 12:00:57 +0300

    create: create a code file print the last digit of number is biger then 5 or less.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 lastD = abs(number) % 10
 if number < 0:
-   lastD = -lastD
+    lastD = -lastD
 if lastD > 5:
     print(f"Last digit of {number} is {lastD} and is greater than 5")
 elif lastD == 0:
