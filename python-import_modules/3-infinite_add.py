d9c8c5ae5c62f0687a5667e9a54b86f1fa0032e7
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-16 21:31:12 +0300

    create: create porgram print that sum all the aurgimnts from the system

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-total = 0
-for i in range(1, len(sys.argv)):
-    total += int(sys.argv[i])
-print(total)
+if __name__ == "__main__":
+    total = 0
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+    print(total)
