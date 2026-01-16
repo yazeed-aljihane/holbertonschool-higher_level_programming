bd2bd30493ea885808ffabb76c6475f0418c9b91
Your Name <you@example.comv>
2026-01-16 18:56:54 +0000

    create: create porgram print the files in hidden module

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    names = sorted(dir(hidden_4))
+    for name in names:
+        if not name.startswith("__"):
+            print(name)
