8e813e7d9824639623c4ce5021cfbf3ecbacc9e8
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-16 21:18:11 +0300

    create: create porgram print the aurgimant from the system

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    print("{} arguments:".format(len(sys.argv) - 1))
+    if len(sys.argv) > 2:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    else:
+        print("{} argument:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
