4ae7f9b40452e29e3b7b989b3b970e072b180be5
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-17 18:32:09 +0300

    create: create porgram  handles basic operation

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-import calculator_1
+from calculator_1 import add, sub, mul, div
 import sys
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if sys.argv[2] == '+':
-        result = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
+        result = add(int(sys.argv[1]), int(sys.argv[3]))
         print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3],
               result))
     elif sys.argv[2] == '-':
-        result = calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
+        result = sub(int(sys.argv[1]), int(sys.argv[3]))
         print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3],
               result))
     elif sys.argv[2] == '*':
-        result = calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
+        result = mul(int(sys.argv[1]), int(sys.argv[3]))
         print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3],
               result))
     elif sys.argv[2] == '/':
-        result = calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
+        result = div(int(sys.argv[1]), int(sys.argv[3]))
         print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3],
               result))
     else:
