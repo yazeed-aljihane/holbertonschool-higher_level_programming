12804e9fe3b9b7a8678d91ffd71a21d7076645c6
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-17 14:40:50 +0300

    create: create porgram prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+check = 1
+for i in range(122, 96, -1):
+    if check == 0:
+        print(chr(i - 32), end="")
+        check = 1
+    elif check == 1:
+        print(chr(i), end="")
+        check = 0
