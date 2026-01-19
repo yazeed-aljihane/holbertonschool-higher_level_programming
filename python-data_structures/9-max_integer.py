bf369d0765e96d6889ab5c9d1541bd075a538140
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-19 11:02:57 +0300

    create: create porgram finds the biggest integer of a list.

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_1 = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] > max_1:
+            max_1 = my_list[i]
+    return max_1
