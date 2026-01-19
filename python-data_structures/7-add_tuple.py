72442b717d36b21e818ddc38829df5abcad45edb
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-19 10:37:51 +0300

    create: create porgram adds 2 tuples.

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 1:
+        tuple_c = tuple_a[0] + tuple_b[0], 0 + tuple_b[1]
+    elif len(tuple_b) == 1:
+        tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + 0
+    elif len(tuple_a) == 1 and len(tuple_b) == 1:
+        tuple_c = tuple_a[0] + tuple_b[0], 0 + 0
+    elif len(tuple_a) == 0:
+        tuple_c = tuple_b
+    elif len(tuple_b) == 0:
+        tuple_c = tuple_a
+    else:
+        tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return tuple_c
