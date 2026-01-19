0ff817e5750d77f5c5e99197ba57767250188388
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-19 09:56:33 +0300

    create: create porgram prints a matrix of integers.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if matrix[i][j] == matrix[i][-1]:
-                print("{:d}".format(matrix[i][j]), end="\n")
+                print("{:d}".format(matrix[i][j]), end="")
                 break
             print("{:d}".format(matrix[i][j]), end=" ")
+        print("\n", end="")
