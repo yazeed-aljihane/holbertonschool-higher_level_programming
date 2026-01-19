ba95cb512944b4a09708ecee4f2a23e614a0e436
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-19 10:39:30 +0300

    create: create porgram adds 2 tuples.

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -12,4 +12,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_c = tuple_a
     else:
         tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    return tuple_c
+    return tuple_c,
