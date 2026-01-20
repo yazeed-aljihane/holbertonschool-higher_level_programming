b4c858b17c0e50a735a8692585f0c45e14413fe1
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-20 08:00:04 +0300

    create: create program that prints a dictionary by ordered keys.

---
+++ python-more_data_structures/6-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+p = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a = {'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
+p(a)
---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,3 @@
+def print_sorted_dictionary(a_dictionary):
+    for i in sorted(a_dictionary):
+        print(f"{i}: {a_dictionary[i]}")
