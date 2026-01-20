7bd20b126c3767929eab89a5dd353458cb79f691
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-20 21:07:33 +0300

    create: create program that that converts a Roman numeral to an integer.
    .

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    d = {'I':1, 'X':10, 'C':100, 'M':1000, 'D':500, 'L':50, 'V':5}
+    d = {'I': 1, 'X': 10, 'C': 100, 'M': 1000, 'D': 500, 'L': 50, 'V': 5}
     total = 0
     for i in range(len(roman_string)):
         current = d.get(roman_string[i], 0)
-        next_val = d.get(roman_string[i + 1], 0) if i + 1 < len(roman_string) else 0
-        if current < next_val:
+        nex = d.get(roman_string[i + 1], 0) if i + 1 < len(roman_string) else 0
+        if current < nex:
             total -= current
         else:
             total += current
