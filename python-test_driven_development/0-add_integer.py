0e2208f42f5b5e731bbcd8e0413fec64f5eb2cbe
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-22 12:03:35 +0300

    create: create program that adds 2 integers.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module for add_integer function."""
+
+
 def add_integer(a, b=98):
     """
     Adds two integers.
@@ -18,5 +20,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
     return int(a) + int(b)
