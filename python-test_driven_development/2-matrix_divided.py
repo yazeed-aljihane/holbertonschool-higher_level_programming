cf428adbfc7a70c782c4b44926db7c84a55d9193
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2026-01-23 07:57:53 +0300

    create: create program that divides all elements of a matrix.

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -23,7 +23,6 @@ def matrix_divided(matrix, div):
     """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
