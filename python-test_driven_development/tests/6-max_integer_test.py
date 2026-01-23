38375cbcff1371ea5a5efada70297e9c9fcff51d
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-01-23 17:05:25 +0300

    create: create program that unit tests for the function max_integer().

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,6 +5,10 @@ class TestMax(unittest.TestCase):
     def test_Max(self):
         self.assertEqual(max_int([]), None)
         self.assertEqual(max_int([13, 14, 56, 565]), 565)
-        self.assertEqual(max_int([-1, 22, -45, 45]), 45)
+        self.assertEqual(max_int([-1]), -1)
+        self.assertEqual(max_int([555, 454 ,7]), 555)
+        self.assertEqual(max_int([555, 654 ,7]), 654)
+        self.assertEqual(max_int([555, -1 ,7]), 555)
+        self.assertEqual(max_int([7]), 7)
         with self.assertRaises(TypeError):
             max_int(["er", 56, 556])
