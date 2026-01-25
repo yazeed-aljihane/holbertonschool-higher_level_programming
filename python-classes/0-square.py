11e0aa845013534a39ccdeb010dcc39f9cf52478
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-01-25 11:40:37 +0300

    create: create program that Write an empty class Square that defines a square:

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines an empty Square class.
+
+The module is designed to demonstrate the basic structure of a class
+in Python as part of a geometry-related project.
+"""
+
+
 class Square:
+    """A class that defines a square.
+
+    This class currently does not have any attributes or methods. It serves
+    as a placeholder for future square-related functionality.
+    """
     pass
