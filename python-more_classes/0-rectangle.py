9f4b0e74f8c40d07b5d0bdf3968bd4cb4f5f8830
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-01-26 22:20:01 +0300

    create: create program that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a Rectangle class.
+
+This module provides a basic template for rectangle objects,
+following the requirements for class definitions.
+"""
+
+
+class Rectangle:
+    """A class used to represent a Rectangle.
+
+    This class is currently a placeholder for defining rectangle
+    attributes and methods in future iterations.
+    """
+    pass
