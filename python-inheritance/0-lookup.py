a2ea52163aef68bb6ba969fb85651b3f528c80b5
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-01-27 19:18:42 +0300

    create: create program that returns the list of available attributes and methods of an object:

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""This module provides a function to lookup object attributes and methods."""
+
+
 def lookup(obj):
     """Returns a list of available attributes and methods of an object.
 
     Args:
-        obj (any): The object to inspect.
+        obj (any): The object to be inspected.
 
     Returns:
-        list: A list of strings representing the object's attributes and methods.
+        list: A list of strings containing the names of the object's 
+        attributes and methods.
     """
     return dir(obj)
