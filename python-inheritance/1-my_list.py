a40d12220bf38ac5b3368c19cae64c3ed81e4d44
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-01-27 19:37:56 +0300

    create: create program that inherits from list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,14 +3,16 @@
 
 This module provides a specialized list with a sorted print method.
 """
+
+
 class MyList(list):
-"""A subclass of list with additional printing capabilities.
+    """A subclass of list with additional printing capabilities.
 
     Attributes:
         No new attributes added.
     """
     def print_sorted(self):
-    """Prints the list in ascending sorted order.
+        """Prints the list in ascending sorted order.
 
         Assumes all elements are of type int.
         """
