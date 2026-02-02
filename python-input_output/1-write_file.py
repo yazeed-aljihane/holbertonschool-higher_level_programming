ecd6bd35c07f3069d3d14bd9e675484e7f60f50d
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-02-02 19:43:34 +0300

    create: create program that  writes a string to a text file (UTF8) and returns the number of characters written

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,18 +3,18 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF-8) and returns the number of characters written.
+    """Writes a string to a text file (UTF-8) and returns characters written.
 
     Args:
-        filename (str): The name of the file to write to. Defaults to an empty string.
-        text (str): The string to be written to the file. Defaults to an empty string.
+        filename (str): The name of the file to write to.  an empty string.
+        text (str): The string to be written to the file.  an empty string.
 
     Returns:
         int: The number of characters written to the file.
 
     Note:
-        This function uses the 'with' statement to ensure the file is closed 
-        after writing. It will overwrite the file if it already exists or 
+        This function uses the 'with' statement to ensure the file is closed
+        after writing. It will overwrite the file if it already exists or
         create it if it does not.
     """
     with open(filename, mode='w', encoding='utf-8') as my_file:
