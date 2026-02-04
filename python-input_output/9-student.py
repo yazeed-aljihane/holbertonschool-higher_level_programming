078b9ab48d320998fc1215dae873bcaa26d8e403
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-02-04 18:51:45 +0300

    create: create program that that defines a student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,10 +1,38 @@
 #!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
 class Student:
-    
+    """Defines a student by first name, last name, and age.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
     def __init__(self, first_name, last_name, age):
+        """Initializes the student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes contained in this
+        list must be retrieved. Otherwise, all attributes are retrieved.
+
+        Args:
+            attrs (list, optional): A list of strings representing attribute
+                names to fetch. Defaults to None.
+
+        Returns:
+            dict: A dictionary containing the student's attributes.
+        """
         return self.__dict__
