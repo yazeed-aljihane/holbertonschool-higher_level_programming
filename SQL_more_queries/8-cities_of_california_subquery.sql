4dec3e440eb822c4688f486d87338da1d1a00727
Your Name <you@example.com>
2026-02-24 03:59:45 +0000

    lists all the cities of California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- Lists all cities of California found in the database hbtn_0d_usa
+SELECT id, name 
+FROM cities 
+WHERE state_id = (
+    SELECT id 
+    FROM states 
+    WHERE name = 'California'
+) 
+ORDER BY id ASC;
