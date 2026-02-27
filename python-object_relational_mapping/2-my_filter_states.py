ce6a4d00b3109dac35d3e3c9bc6edc577c694b3f
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-02-27 07:33:12 +0300

    list all state with name given

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This script lists all states from the database hbtn_0e_0_usa where
+name matches the argument, safe from MySQL injections.
+"""
+import MySQLdb
+import sys
+
+
+def list_all():
+    """Connects to the database and selects states based on user input.
+
+    The script uses parameterized queries to prevent SQL injection attacks.
+    Arguments are retrieved from the command line.
+    """
+    if len(sys.argv) != 5:
+        return
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+        cur = db.cursor()
+
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+        cur.execute(query, (sys.argv[4],))
+
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+        cur.close()
+        db.close()
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    list_all()
