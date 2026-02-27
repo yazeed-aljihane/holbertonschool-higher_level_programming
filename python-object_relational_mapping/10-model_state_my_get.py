fd619614ddd9efedcf523dbdab2505e9c19f1122
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-02-27 23:59:19 +0300

    Get a state

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_search():
+    """Connects to the database and filters states containing 'a'.
+
+    The result is sorted by states.id in ascending order.
+    """
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]
+    )
+
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(
+        name=sys.argv[4]
+    ).order_by(State.id).first()
+    if state:
+        print("{}".format(state.id))
+    else:
+        print('Not found')
+
+    session.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 5:
+        list_search()
