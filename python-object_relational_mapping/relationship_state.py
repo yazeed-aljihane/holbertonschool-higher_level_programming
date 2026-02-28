d56c829d7c4d196b829be12b77bcd401a783e9dc
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-02-28 14:23:56 +0300

    City relationship

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Contains the class definition of a State with relationship.
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from relationship_city import Base, City
