bd5c81fc944e92a68628caf0d20753bd5d7a0b86
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-03-09 13:44:54 +0300

    Add 8-square.js to print a square of 'X' characters based on user input

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+const num = parseInt(argv[2]);
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let j = 0; j < num; j++) {
+    console.log('X'.repeat(num));
+  }
+}
