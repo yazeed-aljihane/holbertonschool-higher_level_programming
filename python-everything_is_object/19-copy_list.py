f7bd97200046b6fe6819774358ddef58474328cf
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-04-05 17:14:04 +0300

    feat: update 18-answer.txt and add new files 19-copy_list.py and 19-main.py with initial content

---
+++ python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def copy_list(my_list):
+    """Returns a copy of a list.
+
+    Args:
+        my_list (list): The list to copy.
+
+    Returns:
+        list: A copy of the input list.
+    """
+    return my_list[:]
